04699d777df12b65e3407621b7b621e1a097e3e1
MYarborough24 <mpyarborough28@gmail.com>
2026-07-27 16:05:40 -0400

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,34 @@
+# Day 2: 30 Days of python programming
+first_name = 'Matthew'
+last_name = 'Yarborough'
+full_name = 'Matthew Yarborough'
+country = 'United States'
+city = 'Leesburg'
+age = '22'
+year = '2026'
+is_married = False
+is_true = False
+is_light_on = True
+x, y, z = 10, 20, 30
 
+print(type(first_name))
+print(len(first_name))
+print(len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = 'num_one' - 'num_two'
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+pi = 3.14
+circum_of_circle = 2 * pi * radius
+radius = float(input("Enter the radius: "))
+area_of_circle = pi * radius ** 2
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
