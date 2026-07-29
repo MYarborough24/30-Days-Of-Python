1427145e00468f0f672fa676b12287d771929650
MYarborough24 <mpyarborough28@gmail.com>
2026-07-29 12:32:09 -0400

    Update lists.py

--- day_5/lists.py
+++ day_5/lists.py
@@ -1,1 +1,22 @@
-
+>>> empty_list = list()
+>>> numbers = [0,1,2,3,4,5]
+>>> print(len(numbers))
+6
+>>> first_item = numbers[0]
+>>> middle_item = numbers[len(numbers)//2]
+>>> last_item = numbers[-1]
+>>> mixed_data_types = ['Matt', 22, '6'0"', 'Single', '510 Fairfield Way SW, Leesburg, VA 20175']
+  File "<stdin>", line 1
+    mixed_data_types = ['Matt', 22, '6'0"', 'Single', '510 Fairfield Way SW, Leesburg, VA 20175']
+                                        ^
+SyntaxError: unterminated string literal (detected at line 1)
+>>> it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+>>> print(it_companies)
+['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+>>> first=it_companies[0]
+>>> middle=it_companies[len(it_companies)//2]
+>>> last=it_companies[-1]
+>>> print(first,middle,last)
+Facebook Apple Amazon
+>>> print(len(it_companies))
+7
